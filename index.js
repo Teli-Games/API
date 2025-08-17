03821ab358dcd0575b73a72393712f72f4ffa1b6
AcronicalYT <ace36501@hotmail.com>
2025-08-17 20:53:10 +0100

    Setup for games api

--- index.js
+++ index.js
@@ -3,26 +3,17 @@ const app = express();
 const PORT = 3001;
 const cors = require('cors');
 const data = require('./json/games.json');
-const projects = require('./json/projects.json');
-const experience = require('./json/experience.json');
-const services = require('./json/services.json');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+    res.send('Welcome to the Teli Games API! Use /games to get the list of games.');
+});
+
+app.get('/games', (req, res) => {
     res.json(data);
-});
-
-app.get('/projects', (req, res) => {
-    res.json(projects);
-});
-
-app.get('/experience', (req, res) => {
-    res.json(experience);
-});
-
-app.get('/services', (req, res) => {
-    res.json(services);
 });
 
 app.listen(PORT, () => {
